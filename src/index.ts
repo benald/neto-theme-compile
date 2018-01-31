fc9e44f93c6cdb769deec66f37866a610c83e7fa
Matt Downs <mattdowns6@gmail.com>
2018-01-31 19:20:09 +1000

    Oops. Here goes version 0.0.3 baby!

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
 export { NetoThemeCompiler, INetoThemeCompilerOptions } from "./lib/compile";
-export { watchAndCompile } from "./lib/gulp";
+export { watchAndCompile as gulpWatchAndCompile } from "./lib/gulp";
