41dbc8b35180e4122f452443cec88b150f441d51
Matt Downs <mattdowns6@gmail.com>
2018-01-31 18:35:15 +1000

    Fully TypeScript bro!

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var compile_1 = require("./lib/compile");
-exports.NetoThemeCompiler = compile_1.NetoThemeCompiler;
-var gulp_1 = require("./lib/gulp");
-exports.watchAndCompile = gulp_1.watchAndCompile;
